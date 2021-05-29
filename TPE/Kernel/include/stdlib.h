57368be4c994e0f41a27d9dff170a23363c4ecb2
mcornidez <67882015+mcornidez@users.noreply.github.com>
2021-05-29 19:30:15 -0300

    Add files via upload

---
+++ TPE/Kernel/include/stdlib.h
@@ -0,0 +1,13 @@
+
+#ifndef STDLIB_H
+#define STDLIB_H
+
+void printf(char * format, ...);
+char * hexaToString(int num, char * str);
+char * decimalToString(int num, char * str);
+char * reverseString(char * str, int len);
+int strlen(char * string);
+char* itoa(int num, char* str, int base);
+
+
+#endif

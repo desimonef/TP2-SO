a4d636453d0e3776ff5d4d82dc9166460c88f880
desimonef <67933006+desimonef@users.noreply.github.com>
2021-05-30 19:54:47 -0300

    Update sysCallDispatcher.h

--- TPE/Kernel/include/sysCallDispatcher.h
+++ TPE/Kernel/include/sysCallDispatcher.h
@@ -6,7 +6,7 @@
 <<<<<<< HEAD
 void sysCallDispatcher (uint64_t rax, uint64_t rdi, uint64_t rsi, uint64_t rdx);
 =======
-oid sysCallDispatcher (uint64_t rax, uint64_t rdi, uint64_t rsi, uint64_t rdx);
+void sysCallDispatcher (uint64_t rax, uint64_t rdi, uint64_t rsi, uint64_t rdx);
 >>>>>>> 664ba25eeda664f8e265c2f59e0b0abdb58c13f8
 void sysRead (char * buff, uint64_t address, uint16_t amount);
 void sysWrite(int fd, char * buff, uint64_t count);

788db82b7419cca0c976072650c5b26a93e2347c
mcornidez <67882015+mcornidez@users.noreply.github.com>
2021-05-30 19:41:57 -0300

    se agregaron funciones

--- TPE/Kernel/include/lib.h
+++ TPE/Kernel/include/lib.h
@@ -5,7 +5,18 @@
 
 void * memset(void * destination, int32_t character, uint64_t length);
 void * memcpy(void * destination, const void * source, uint64_t length);
+char * toHexa (long long number, int bytes);
+char *intToHexa(long long num, char *str, int bytes);
+int hexaToInt(char* num);
+char* itoa(int num, char* str, int base);
+void reverse(char str[], int length);
+void printf(char * format, ...);
+int strlen(char * string);
+
 
 char *cpuVendor(char *result);
+long getRegister(int index);
+void dumpMem(char * buff, int address, int bytes);
+int asmGetByte(int pos);
 
 #endif

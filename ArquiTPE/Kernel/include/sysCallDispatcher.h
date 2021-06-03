11cfacfc833d92e9f210a6d85faeac4aab5964d2
desimonef <67933006+desimonef@users.noreply.github.com>
2021-06-03 19:04:34 -0300

    Update sysCallDispatcher.h

--- ArquiTPE/Kernel/include/sysCallDispatcher.h
+++ ArquiTPE/Kernel/include/sysCallDispatcher.h
@@ -3,13 +3,13 @@
 
 #include <stdint.h>
 
-void sysCallDispatcher (uint64_t rax, uint64_t rdi, uint64_t rsi, uint64_t rdx);
-void sysRead (int fd, char * buff, uint16_t amount);
-void sysWrite(int fd, char * buff, uint64_t count);
+uint64_t sysCallDispatcher (uint64_t rax, uint64_t rdi, uint64_t rsi, uint64_t rdx);
+uint64_t sysRead (int fd, char * buff, uint16_t amount);
+uint64_t sysWrite(int fd, char * buff, uint64_t count);
 uint64_t sysGetRegs(uint64_t buffer, uint64_t rdx, uint64_t rcx);
 void sysGetMem(char * buff, uint64_t address, uint64_t amount);
 uint64_t sysGetDateTime(uint64_t id, uint64_t rdx, uint64_t rcx);
-void sysClearScreen(char * buff, uint64_t address, uint64_t amount);
+void sysClearScreen(uint64_t mode, uint64_t address, uint64_t amount);
 void sysGetDateTimeBuff(uint64_t id, uint64_t buff, uint64_t rcx);
 
 #endif

5a75b7b57f11a1119e9ae0fcaa86f7cd4132ecff
desimonef <67933006+desimonef@users.noreply.github.com>
2021-06-03 19:35:09 -0300

    Update keyboard.c

--- ArquiTPE/Kernel/keyboard.c
+++ ArquiTPE/Kernel/keyboard.c
@@ -9,16 +9,22 @@
 #define SHIFT_RELEASE 170
 #define CAPS_PRESS 58
 #define ENTER 28
+#define ENTER_RELEASE 156
 #define TAB 15
-#define UP 0
-#define LEFT 1
-#define RIGHT 2
-#define DOWN 3
+#define UP 72
+#define UP_RELEASE 200
+#define LEFT 75
+#define LEFT_RELEASE 203
+#define RIGHT 77
+#define RIGHT_RELEASE 205
+#define DOWN 80
+#define DOWN_RELEASE 208
 
 static char keyboardBuffer[INPUT_BUFFER];
 static unsigned int bufferSize = 0;
 static unsigned int readIndex = 0;
 static int shift = 0;
+static int unread = 0;
 
 static const char asciiTable[] = {
     0, 0, '1', '2',
@@ -28,7 +34,7 @@ static const char asciiTable[] = {
     'q', 'w', 'e', 'r',
     't', 'y', 'u', 'i',
     'o', 'p', '`', '+',
-    0, 0, 'a', 's',
+    '\n', 0, 'a', 's',
     'd', 'f', 'g', 'h',
     'j', 'k', 'l', 'ñ',
     '´', 'ç', 0, '<',
@@ -36,8 +42,7 @@ static const char asciiTable[] = {
     'b', 'n', 'm', ',',
     '.', '-', 0, 0, 0,
     ' ', 0, 0, 0, 0,
-    0, 0, 0, 0
-  };
+    0, 0, 0, 0};
 
 static const char asciiTableShift[] = {
     0, 0, '!', '"',
@@ -63,7 +68,15 @@ int codeMatchesAscii(int scancode){
 
 uint8_t getMatchingAscii(int scancode){
   isShiftPressed(scancode);
-  isArrowKey(scancode);
+  int isArrow = isArrowKey(scancode);
+  if (scancode == ENTER || scancode == ENTER_RELEASE)
+  {
+    setMove(1);
+  }
+  else if (isArrow == 0)
+  {
+    setMove(0);
+  }
   if (codeMatchesAscii(scancode)) {
     if (shift == 0)
       return asciiTable[scancode];
@@ -72,8 +85,8 @@ uint8_t getMatchingAscii(int scancode){
   }
   else if (scancode == TAB)
   {
-    //int current = getCursor();
-    //setCursor(current + 5);
+    int current = getCursor();
+    setCursor(current + 5);
   }
   return 0;
 }
@@ -85,22 +98,22 @@ void isShiftPressed(int scancode)
   
 }
 
-void isArrowKey(int scancode) {
+int isArrowKey(int scancode) {
+  
   switch (scancode)
   {
-  case UP:
-    //changeScreen(0);
-    break;
-  case LEFT:
-    //moveCursor(-1);
-    break;
-  case RIGHT:
-    //moveCursor(1);
-    break;
-  case DOWN:
-    //changeScreen(1);
-    break;
+    case UP:
+      changeScreen(0);
+    case LEFT:
+      moveCursor(-1);
+    case RIGHT:
+      moveCursor(1);
+    case DOWN:
+      changeScreen(1);
   }
+  if (scancode == UP || scancode == DOWN || scancode == RIGHT || scancode == LEFT || scancode == UP_RELEASE || scancode == LEFT_RELEASE || scancode == RIGHT_RELEASE || scancode == DOWN_RELEASE)
+    return 1;
+  return 0;
 }
 
 void checkIndex() {
@@ -110,24 +123,33 @@ void checkIndex() {
   }
 }
 
-int readFromKeyboard(char * dest, int amount) {
-    int dim = 0;
-    while(amount > 0) {
-        dest[dim++] = keyboardBuffer[readIndex++];
-        checkIndex();
-        amount--;
-    }
-    return dim;
+int keyboardRead(char *ret, int count) {
+  if(unread <= 0) {
+    return -1;
+  }
+    
+  int size = 0;
+  for(int i=0; i<count; i++) {
+    ret[size] = keyboardBuffer[readIndex];
+    size++;
+    readIndex++;
+    checkIndex();
+    unread--;
+  }
+  ret[size] = 0;
+  return size;
 }
 
 void store(char c) {
     keyboardBuffer[bufferSize] = c;
-    bufferSize = (bufferSize+1)%INPUT_BUFFER; 
+    bufferSize = (bufferSize+1)%INPUT_BUFFER;
+    unread++;
+    
 }
 
 void keyboard_handler(){
-    int scanCode = readKey();
-    char c = getMatchingAscii(scanCode);
-    store(c);
-    ncPrintChar(c);
+  int scanCode = readKey();
+  char c = getMatchingAscii(scanCode);
+  store(c);
+
 }

8204fed677783abb415f43edf7fd3e896b4a2f38
desimonef <67933006+desimonef@users.noreply.github.com>
2021-06-06 13:53:58 -0300

    Update keyboard.h

--- ArquiTPE/Kernel/include/keyboard.h
+++ ArquiTPE/Kernel/include/keyboard.h
@@ -6,11 +6,11 @@
 int codeMatchesAscii(int scancode);
 int getScanCode();
 int isLetter(char c);
-uint8_t getMatchingAscii(int scancode);
+char getAscii(int scancode);
 void onKeyPressed(char chr); 
 int keyboardRead(char *ret, int count);
 void keyboard_handler();
-void isShiftPressed(int scancode);
-int isArrowKey(int scancode) ;
+void shiftPressing(int scancode);
+int arrows(int scancode);
 
 #endif

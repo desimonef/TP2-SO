c82c065eda1e9d481d6a5b4c8fbecdea66f8a9b8
desimonef <67933006+desimonef@users.noreply.github.com>
2021-06-06 13:52:38 -0300

    Doble pantalla :)

--- ArquiTPE/Kernel/keyboard.c
+++ ArquiTPE/Kernel/keyboard.c
@@ -19,6 +19,7 @@
 #define RIGHT_RELEASE 205
 #define DOWN 80
 #define DOWN_RELEASE 208
+#define ARROWS 224
 
 static char keyboardBuffer[INPUT_BUFFER];
 static unsigned int bufferSize = 0;
@@ -62,74 +63,111 @@ static const char asciiTableShift[] = {
     ' ', 0, 0, 0, 0,
     0, 0, 0, 0};
 
-int codeMatchesAscii(int scancode){
+
+
+char getAscii(int scancode)
+{
+  shiftPressing(scancode);
+  int arrow = isArrow(scancode);
+  if (arrow == 0)
+  {
+    screenMove(scancode);
+  }
+
+  if (codeMatchesAscii(scancode))
+  {
+      if (shift == 0)
+        return asciiTable[scancode];
+      else
+        return asciiTableShift[scancode];
+  }
+
+  if (scancode == TAB)
+    moveCursor(5);
+
+  return 0;
+}
+
+int codeMatchesAscii(int scancode)
+{
   return scancode >= 0 && scancode <= 57 && asciiTable[scancode] != 0;
 }
 
-uint8_t getMatchingAscii(int scancode){
-  isShiftPressed(scancode);
-  int isArrow = isArrowKey(scancode);
-  if (scancode == ENTER || scancode == ENTER_RELEASE)
-  {
-    //setMove(1);
+int isArrow(int scancode)
+{
+  int toRet = 0;
+  switch(scancode) {
+    case UP:
+      changeScreen(0);
+      toRet = 1;
+      break;
+    case DOWN: 
+      changeScreen(1);
+      toRet = 1;
+      break;
+    case LEFT:
+      moveCursor(-1);
+      toRet = 1;
+      break;
+    case RIGHT:
+      moveCursor(1);
+      toRet = 1;
+      break;
+    case UP_RELEASE:
+      toRet = 1;
+      break;
+    case DOWN_RELEASE:
+      toRet = 1;
+      break;
+    case LEFT_RELEASE:
+      toRet = 1;
+      break;
+    case RIGHT_RELEASE:
+      toRet = 1;
+      break;
+    case ARROWS:
+      toRet = 1;
+      break;
   }
-  else if (isArrow == 0)
-  {
-   // setMove(0);
-  }
-  if (codeMatchesAscii(scancode)) {
-    if (shift == 0)
-      return asciiTable[scancode];
-    else
-      return asciiTableShift[scancode];
-  }
-  else if (scancode == TAB)
-  {
-    int current = getCursor();
-    setCursor(current + 5);
-  }
-  return 0;
+  
+  return toRet;
 }
 
-void isShiftPressed(int scancode)
+void screenMove(int scancode)
+{
+  if (scancode == ENTER || scancode == ENTER_RELEASE)
+  {
+    setMove(1);
+  }
+  else
+  {
+    setMove(0);
+  }
+}
+
+void shiftPressing(int scancode)
 {
   if (scancode == SHIFT_PRESS || scancode == SHIFT_RELEASE || scancode == CAPS_PRESS)
     shift = 1 - shift;
-  
 }
-
-int isArrowKey(int scancode) {
-  
-  switch (scancode)
-  {
-    case UP:
-      changeScreen(0);
-    case LEFT:
-      moveCursor(-1);
-    case RIGHT:
-      moveCursor(1);
-    case DOWN:
-      changeScreen(1);
-  }
-  if (scancode == UP || scancode == DOWN || scancode == RIGHT || scancode == LEFT || scancode == UP_RELEASE || scancode == LEFT_RELEASE || scancode == RIGHT_RELEASE || scancode == DOWN_RELEASE)
-    return 1;
-  return 0;
-}
-
-void checkIndex() {
+void checkIndex()
+{
   if (readIndex >= INPUT_BUFFER)
   {
     readIndex = readIndex % INPUT_BUFFER;
   }
 }
 
-int keyboardRead(char *ret, int count) {
-  if(unread <= 0) {
+int keyboardRead(char *ret, int count)
+{
+  if (unread <= 0)
+  {
     return -1;
   }
-    
+
   int size = 0;
-  for(int i=0; i<count; i++) {
+  for (int i = 0; i < count; i++)
+  {
     ret[size] = keyboardBuffer[readIndex];
     size++;
     readIndex++;
@@ -140,16 +178,17 @@ int keyboardRead(char *ret, int count) {
   return size;
 }
 
-void store(char c) {
-    keyboardBuffer[bufferSize] = c;
-    bufferSize = (bufferSize+1)%INPUT_BUFFER;
-    unread++;
-    
+void store(char c)
+{
+  keyboardBuffer[bufferSize] = c;
+  bufferSize = (bufferSize + 1) % INPUT_BUFFER;
+  unread++;
 }
 
-void keyboard_handler(){
-  int scanCode = readKey();
-  char c = getMatchingAscii(scanCode);
+void keyboard_handler()
+{
+  int scancode = readKey();
+  setStart();
+  char c = getAscii(scancode);
   store(c);
-
 }
